5d870962e1e48b6bc9e8470a05244f3a3eb9a394
vliubko <vliubko@student.unit.ua>
2018-04-18 16:29:37 +0300

    [NORME] fix

--- src/bi_cd.c
+++ src/bi_cd.c
@@ -1,90 +1,93 @@
-//
-// Created by Vadym LIUBKO on 4/16/18.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bi_cd.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vliubko <vliubko@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/18 16:05:50 by vliubko           #+#    #+#             */
+/*   Updated: 2018/04/18 16:29:19 by vliubko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "minishell.h"
 
-
-char    *tild_replace_home(char *line)
+char			*tild_replace_home(char *line)
 {
-    char *new_line;
-    char *home;
-    char *find_after;
+	char *new_line;
+	char *home;
+	char *find_after;
 
-    if (!(find_after = ft_strchr(line, '~')))
+	if (!(find_after = ft_strchr(line, '~')))
 		return (line);
 	find_after++;
-    if (*find_after == '\0' || *find_after == '/')
-    {
-        home = get_env_value_by_name("HOME");
-        new_line = ft_strjoin_free(home, find_after, 0);
-        ft_strdel(&line);
-        return (new_line);
-    }
+	if (*find_after == '\0' || *find_after == '/')
+	{
+		home = get_env_value_by_name("HOME");
+		new_line = ft_strjoin_free(home, find_after, 0);
+		ft_strdel(&line);
+		return (new_line);
+	}
 	return (line);
 }
 
-
-static int    cd_access_check(char *path, int count_args)
+static int		cd_access_check(char *path, int count_args)
 {
-    struct stat		f;
+	struct stat		f;
 	char			buf[MAXPATHLEN];
 
 	ft_strcpy(buf, "cd: ");
-    if (count_args > 1)
-    {
-        ft_putendl("Too many arguments.");
-    }
-    else if (lstat(path, &f) == -1)
-    {
+	if (count_args > 1)
+		ft_putendl("Too many arguments.");
+	else if (lstat(path, &f) == -1)
+	{
 		ft_putstr(ft_strcat(buf, path));
-        ft_putendl(": No such directory.");
-    }
-    else if (S_ISREG(f.st_mode))
-    {
+		ft_putendl(": No such directory.");
+	}
+	else if (S_ISREG(f.st_mode))
+	{
 		ft_putstr(ft_strcat(buf, path));
-        ft_putendl(": Not a directory.");
-    }
-    else if (!(f.st_mode & S_IXUSR))
-    {
+		ft_putendl(": Not a directory.");
+	}
+	else if (!(f.st_mode & S_IXUSR))
+	{
 		ft_putstr(ft_strcat(buf, path));
-        ft_putendl(": Permission denied.");
-    }
+		ft_putendl(": Permission denied.");
+	}
 	else
-    	return (0);
+		return (0);
 	return (1);
 }
 
-int     cd_standart (char *path)
+int				cd_standart(char *path)
 {
 	char dir[MAXPATHLEN];
 
 	getcwd(dir, MAXPATHLEN);
 	change_env_value_by_name("OLDPWD", dir);
-    chdir(path);
+	chdir(path);
 	getcwd(dir, MAXPATHLEN);
 	change_env_value_by_name("PWD", dir);
-    ft_strdel(&path);
-    return (1);
+	ft_strdel(&path);
+	return (1);
 }
 
-int     bi_cd(char **arg)
+int				bi_cd(char **arg)
 {
-    char    *new_path;
-    int     count_args;
+	char	*new_path;
+	int		count_args;
 
-    count_args = ft_count_elems(NULL, arg, 2);
-
-    if (count_args == 0)
-        new_path = get_env_value_by_name("HOME");
-    else if (ft_strequ("-", arg[0]) && count_args == 1)
-        new_path = get_env_value_by_name("OLDPWD");
-    else
-        new_path = ft_strdup(arg[0]);
-    if (cd_access_check(new_path, count_args) == 1)
+	count_args = ft_count_elems(NULL, arg, 2);
+	if (count_args == 0)
+		new_path = get_env_value_by_name("HOME");
+	else if (ft_strequ("-", arg[0]) && count_args == 1)
+		new_path = get_env_value_by_name("OLDPWD");
+	else
+		new_path = ft_strdup(arg[0]);
+	if (cd_access_check(new_path, count_args) == 1)
 	{
 		ft_strdel(&new_path);
 		return (1);
 	}
-    return (cd_standart(new_path));
+	return (cd_standart(new_path));
 }
